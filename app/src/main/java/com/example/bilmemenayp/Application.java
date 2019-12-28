6e09b849b9c857a9341755c06c4d901cadbeb9d0
Onur Yarar <onurr.yarar@hotmail.com>
2019-12-28 03:03:07 +0300

    service path changed

--- app/src/main/java/com/example/bilmemenayp/Application.java
+++ app/src/main/java/com/example/bilmemenayp/Application.java
@@ -1,7 +1,7 @@
 package com.example.bilmemenayp;
 
 public class Application {
-    private String ServiceUrl = "http://10.60.81.85/BA/";
+    private String ServiceUrl = "http://10.60.81.85/BAServices/";
 
     public String getServiceUrl() {
         return ServiceUrl;
